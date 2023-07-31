9487cb2388de0aa202b26a39c655ae31f8788228
sharathsl <sharth.sl@gmail.com>
2023-07-31 20:14:48 +0530

    Adding create_k8s_cluster

---
+++ create_k8s_cluster/vars.tf
@@ -0,0 +1,19 @@
+variable "subnet1" {
+    type = string
+    default = "subnet-052a2d5114164b3fb"
+}
+
+variable "subnet2" {
+    type = string
+    default = "subnet-05cda4ccfc2723458"
+}
+
+variable "subnet3" {
+    type = string
+    default = "subnet-0c300575af3bc52a8"
+}
+
+variable "subnet4" {
+    type = string
+    default = "subnet-0abcaba2fe967a258"
+}
